31ed9c99163b9e11108f5ad06f06d47d3c445a17
-_- <majest228@gmail.com>
2025-06-25 18:21:51 +0500

    Страница recovery

--- src/widgets/recovery-password/ui/Info.tsx
+++ src/widgets/recovery-password/ui/Info.tsx
@@ -7,6 +7,15 @@ export const Info = () => {
 		<div className={styles['info']}>
 			<Image src={logo} alt='logo' />
 			<h3 className={styles['info-title']}>Восстановление пароля</h3>
+			<div className={styles['info-statuses']}>
+				<div className={styles['info-statuses__item']}>
+					<div className={styles['info-statuses__item__circle']}></div>
+					<span className={styles['info-statuses__item__title']}>
+						Valid your phone
+					</span>
+				</div>
+				<div className={styles['info-statuses__item__border']}></div>
+			</div>
 		</div>
 	)
 }
